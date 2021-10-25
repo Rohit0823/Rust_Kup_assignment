61ec78d7fc472b7112f34571b01051ca2dc335cb
Rohit0823 <87650330+Rohit0823@users.noreply.github.com>
2021-10-25 12:37:23 +0530

    Update check_even.rs

--- Assignments/assignment_7/src/question1/check_even.rs
+++ Assignments/assignment_7/src/question1/check_even.rs
@@ -2,11 +2,11 @@
 ///
 /// #Arguments
 ///
-/// value : value object check the condition.
+/// value : taking i32 as input
 ///
 /// #Return
 ///
-/// return handle error and use value.
+/// Return Result enum which is used to handle error and value
 pub fn is_even(value: i32) -> Result<String, String> {
     if value % 2 == 0 {
         Ok("Even".to_string())
@@ -14,15 +14,15 @@ pub fn is_even(value: i32) -> Result<String, String> {
         Err("Not even".to_string())
     }
 }
-/// check_test function check the value object.
+/// check_test function is used to handle the response of a function
 ///
 /// #Arguments
 ///
-/// result control the number-value
+/// value is a integer of i32 type Whose check even or not
 ///
 /// #Return
 ///
-/// return handle error and give output String.
+/// Return String which handle error and value of output of function.
 pub fn check_test(value: i32) -> String {
     let result = is_even(value);
 
