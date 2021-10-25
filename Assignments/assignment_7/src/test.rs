b47f3bbc3d165f2eb8719ac05f6d0eff56051034
Rohit0823 <87650330+Rohit0823@users.noreply.github.com>
2021-10-25 22:48:57 +0530

    Update test.rs

--- Assignments/assignment_7/src/test.rs
+++ Assignments/assignment_7/src/test.rs
@@ -3,17 +3,27 @@ pub mod test {
     use crate::question1::check_even::check_test;
 
     #[test]
-    pub fn test1() {
-        assert_eq!(check_test(4), "Even");
+    fn check_even_success_first() {
+        assert_eq!(check_test(18), "Even");
     }
-
     #[test]
-    pub fn test2() {
-        assert_ne!(check_test(0), "Odd");
+    fn check_even_success_second() {
+        assert_eq!(check_test(-24), "Even");
     }
-
     #[test]
-    pub fn test3() {
-        assert_ne!(check_test(5), "Even");
+    fn check_even_failure_first() {
+        assert_eq!(check_test(13), "Invalid");
+    }
+    #[test]
+    fn check_even_failure_second() {
+        assert_eq!(check_test(17), "Invalid");
+    }
+    #[test]
+    fn check_even_success_third() {
+        assert_eq!(check_test(0), "Even");
+    }
+    #[test]
+    fn check_even_failure_third() {
+        assert_eq!(check_test(-45), "Invalid");
     }
 }
