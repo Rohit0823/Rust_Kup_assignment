b64a6b06b44758aa28c5ea1df4ffafefaabdaec3
Rohit0823 <87650330+Rohit0823@users.noreply.github.com>
2021-11-22 19:29:42 +0530

    Update third_odd.rs

--- Assignments/assign_10/src/questions/third_odd.rs
+++ Assignments/assign_10/src/questions/third_odd.rs
@@ -35,7 +35,7 @@ pub fn third_odd(iterable: Store) -> Result<i32, String> {
 ///
 /// #Return
 ///
-/// It can return the Result in the form of T,E variants which can handle the error
+/// It return the Result <T,E>
 
 pub fn find_odd(iterable: Store, iterator: i32) -> i32 {
     info!("finds the number");
