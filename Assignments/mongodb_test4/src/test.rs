dbfb5ba886442bc469a61b0badc48ff778743ccd
Rohit0823 <rohit.verma@knoldus.com>
2021-12-15 11:50:24 +0530

    mongodb_with test4 added

---
+++ Assignments/mongodb_test4/src/test.rs
@@ -0,0 +1,22 @@
+
+pub fn add_user(name: &str) -> String {
+    format!("new_pets {}", name)
+}
+pub use crate::mongodb_rust::_add_user;
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn insert_name_success() {
+        let result = add_user("mikku");
+        assert!(result.contains("mikku"));
+    }
+    #[test]
+    fn insert_name_failure() {
+        let result = add_user("boxer");
+        assert!(result.contains("boxer"),
+                "boxer is not inserted '{}'",
+                result
+        );
+    }
+}
