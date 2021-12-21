4869470ea0aebca65e01ae8695d6f45a5ba271af
Rohit0823 <rohit.verma@knoldus.com>
2021-12-21 19:18:21 +0530

    hit google end point task1 added

---
+++ Assignments/google_task1/src/main.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+
+/// main is a function that can use to get the URL path of the request
+///
+/// #Arguments
+///
+/// No Arguments
+///
+/// #Return
+///
+/// Return Result<()> type
+fn main() -> reqwest::Result<()> {
+    env_logger::init();
+    log::info!("starting");
+    let content = reqwest::blocking::get("https://mocki.io/v1/82ddb819-004f-4c1c-ac7c-7b2a9157c058")?
+        .json::<HashMap<String, String>>()?;
+    println!("{:#?}", content);
+    Ok(())
+}
